3ecded7297b612e34e60a600cf23259e30ed406b
Zhang Zeyu <JerryZhang20010417@outlook.com>
2022-03-20 01:15:48 +0800

    Finish half of FlightReader.

--- Kiosk/src/dbReader/FlightReader.java
+++ Kiosk/src/dbReader/FlightReader.java
@@ -1,5 +1,48 @@
 package dbReader;
 
+/**
+ * This class is a tool that can help you search and get values of flight info quickly.
+ *
+ * @author Zhang Zeyu
+ * @date 2022/3/20
+ * @version 1.0
+ */
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+import java.io.*;
+
 public abstract class FlightReader {
 
+    public int indexOf(String idFlight) {
+        JSONObject jobj = JSON.parseObject(readJsonFile("DB/flight.json"));
+        JSONArray arr = jobj.getJSONArray("flight");
+        int index = 0;
+        while(arr.getJSONObject(index).getString("idFlight").equals(idFlight)) { index++; }
+        return index;
+    }
+
+    public static String readJsonFile(String fileName) {
+        String jsonStr = "";
+        try {
+            File jsonFile = new File(fileName);
+            FileReader fileReader = new FileReader(jsonFile);
+
+            Reader reader = new InputStreamReader(new FileInputStream(jsonFile),"utf-8");
+            int ch = 0;
+            StringBuffer sb = new StringBuffer();
+            while ((ch = reader.read()) != -1) {
+                sb.append((char) ch);
+            }
+            fileReader.close();
+            reader.close();
+            jsonStr = sb.toString();
+            return jsonStr;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
