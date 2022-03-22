991a3f00c7d78da3c7adbdf70c6414263e2b5ded
Zhang Zeyu <997130886@qq.com>
2022-03-23 01:26:07 +0800

    Add javadocs first.

--- Kiosk/src/card/TypeIdLoginCard.java
+++ Kiosk/src/card/TypeIdLoginCard.java
@@ -1,4 +1,21 @@
 package card;
 
+/**
+ * This class returns a panel of typing login card.
+ *
+ * @author Zhang Zeyu
+ * @date 2022/3/23
+ * @version 1.0
+ */
+
 public class TypeIdLoginCard {
+
+    /**
+     *
+     * @return "-1": Authentication failed; "0": No booking number; else: Booking number.
+     */
+    public String getBookingNumByTyping() {
+        String bookingNum = "";
+        return bookingNum;
+    }
 }
