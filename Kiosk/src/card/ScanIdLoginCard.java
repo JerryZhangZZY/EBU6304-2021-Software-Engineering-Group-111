4774c794c65856088b5c7c6555379166eac1c5a8
Zhang Zeyu <997130886@qq.com>
2022-03-23 01:28:46 +0800

    Achieve reading card function(simulation).

--- Kiosk/src/card/ScanIdLoginCard.java
+++ Kiosk/src/card/ScanIdLoginCard.java
@@ -1,18 +1,37 @@
 package card;
 
+import dbReader.PassengerFlightReader;
+import idCardReader.IdCardReader;
+
 import javax.swing.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This class returns a panel of scanning login card.
+ *
+ * @author Zhang Zeyu
+ * @date 2022/3/22
+ * @version 1.0
  */
 
-public class ScanIdLoginCard extends JPanel {
+public abstract class ScanIdLoginCard extends JPanel {
+
     /**
-     *
-     * @return "-1": Scan failed; "0": No booking number; else: Booking number.
+     * Scan the id card and give result back.
+     * @return [F,F,F]: Scan failed; []: No booking number; [bn1]/[bn1,bn2]: Booking number(s).
      */
-    public String getBookingNumByScanning() {
-        String bookingNum = "";
-        return bookingNum;
+    public static List<String> getBookingNumByScanning() {
+        List<String> list = new ArrayList<>();
+        try {
+            list.addAll(PassengerFlightReader.getBookingNumByPassengerId(IdCardReader.readId()));
+            return list;
+        } catch (IOException e) {
+//            e.printStackTrace();
+            for(int i = 0; i < 3; i++)
+                list.add("F");
+            return list;
+        }
     }
 }
