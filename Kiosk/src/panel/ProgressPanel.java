21fb4b1e6b91bbeb11763dc4078a355bda6b4cba
ZaitianWang <wangzaitian@outlook.com>
2022-03-22 14:42:02 +0800

    enable progress panel for seat and meal choosing

--- Kiosk/src/panel/ProgressPanel.java
+++ Kiosk/src/panel/ProgressPanel.java
@@ -1,38 +1,65 @@
 package panel;
 
+import card.SeatSelectionCard;
+
 import java.awt.Color;
-import java.awt.Image;
 import java.awt.Rectangle;
 
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 
+/**
+ * @version 1.0
+ * @author zaitian
+ * @date 3/22
+ * panel that holds cards for customers to choose their meal, seat, etc.
+ */
 public class ProgressPanel extends JPanel {
-
+    private JPanel progressPanel;
+    private JLabel progressLabel;
+    private JPanel cardsPanel;
+    private int progress;
 	/**
 	 * Create the panel.
+     * @param progress which step have customers reached
+     *                 1 for flight, 2 for seat
+     *                 3 for meal, 4 for confirm and pay
 	 */
-	public ProgressPanel() {
+	public ProgressPanel(int progress) {
+	    this.progress = progress;
+
 		setBounds(new Rectangle(0, 0, 1920, 880));
         setBackground(Color.PINK);
         setLayout(null);
         setSize(1920, 880);
-        
-        JPanel progressPanel = new JPanel();
+        /*
+        left panel to hold progress indicator
+         */
+        progressPanel = new JPanel();
         progressPanel.setBounds(0, 0, 320, 880);
         add(progressPanel);
         progressPanel.setLayout(null);
-        
-        JLabel progressLabel = new JLabel("New label");
+        /*
+        progress indicator
+         */
+        //TODO switch case determining which progress indicator to use
+        progressLabel = new JLabel("New label");
         ImageIcon progressImage = new ImageIcon("D:\\Desktop\\testProgress320800.png");
         progressLabel.setIcon(progressImage);
         progressLabel.setBounds(10, 40, 300, 800);
         progressPanel.add(progressLabel);
-        
-        JPanel panel = new JPanel();
-        panel.setBackground(Color.GRAY);
-        panel.setBounds(318, 0, 1600, 880);
-        add(panel);
-	}
+        /*
+        right panel to hold option cards
+         */
+        cardsPanel = new JPanel();
+        cardsPanel.setBackground(Color.PINK);
+        cardsPanel.setBounds(320, 0, 1600, 880);
+        cardsPanel.setLayout(null);
+        add(cardsPanel);
+
+    }
+	public void loadCards(JPanel cards){
+	    cardsPanel.add(cards, 0);
+    }
 }
--- Kiosk/src/panelTest/ProgressPanelTest.java
+++ Kiosk/src/panelTest/ProgressPanelTest.java
@@ -2,6 +2,7 @@ package panelTest;
 
 import javax.swing.JPanel;
 
+import card.SeatSelectionCard;
 import frame.*;
 import panel.*;
 
@@ -10,7 +11,11 @@ public class ProgressPanelTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MainFrame testMainFrame = new MainFrame(1);
-        JPanel testProgressPanel = new ProgressPanel();
+        ProgressPanel testProgressPanel = new ProgressPanel(0);
+        testProgressPanel.loadCards(new SeatSelectionCard( "AC0001",
+                "Normal", "Legroom Pro",
+                "Legroom Max", "Legroom Ultra",
+                0, 10, 20, 50));
         testMainFrame.loadPanel(testProgressPanel);
         testMainFrame.displayComponents(true, true, true);
         testMainFrame.setVisible(true);
