740a0c4b809ed94f60d4229334aace902af24a59
ZaitianWang <wangzaitian@outlook.com>
2022-03-23 15:47:45 +0800

    trivial

---
+++ Kiosk/src/mainTest/ControlTest.java
@@ -0,0 +1,24 @@
+package mainTest;
+
+import org.junit.jupiter.api.Test;
+import tests.TestFrame;
+
+import javax.swing.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControlTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+
+}
