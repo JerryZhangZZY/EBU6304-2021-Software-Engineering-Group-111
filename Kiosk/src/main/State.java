2ec9d361c0026d6f5c6d4c09046145984b35100d
Zhang Zeyu <997130886@qq.com>
2022-03-24 19:09:48 +0800

    Add temp vars.

--- Kiosk/src/main/State.java
+++ Kiosk/src/main/State.java
@@ -9,22 +9,28 @@ package main;
  * at the final page. Add static variables
  * as you need it. Do remember to
  * add getter/setter and avoid name-collision
+ *
+ * @author Zhang Zeyu
+ * @date 2022/3/24
+ * @version 1.1
+ * Add temp vars.
  */
 public abstract class State {
-    static private int pc = 0;
-    static private String passengerName = "passenger";
+    private static int pc = 0;
+    private static String passengerName = "passenger";
+    private static String bookingNum;
+    private static String idPassengerFlight;
+    private static String idFlight;
+    private static String seat;
+    private static int seatPre; //[0, 1, 2, 3]
+    private static int meal;    //[0, 1, 2]
+    private static boolean mealPre1;
+    private static boolean mealPre2;
+    private static boolean mealPre3;
 
+    public static String getPassengerName() { return passengerName; }
+    public static int getPc() { return pc; }
 
-    public static String getPassengerName() {
-        return passengerName;
-    }
-    public static void setPassengerName(String passengerName) {
-        State.passengerName = passengerName;
-    }
-    static public int getPc(){
-        return pc;
-    }
-    static public void setPc(int pcNext){
-        pc = pcNext;
-    }
+    public static void setPassengerName(String passengerName) { State.passengerName = passengerName; }
+    public static void setPc(int pcNext) { pc = pcNext; }
 }
