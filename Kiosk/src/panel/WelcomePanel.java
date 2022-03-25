62c6feca9d86353e42be09beb10c2be1ffd5835b
Zhang Zeyu <997130886@qq.com>
2022-03-26 00:15:20 +0800

    Appearance improvement.

--- Kiosk/src/panel/WelcomePanel.java
+++ Kiosk/src/panel/WelcomePanel.java
@@ -12,6 +12,11 @@ import java.awt.event.MouseEvent;
  * @date 3/24
  * @author zaitian
  * the panel that displays when kiosk is vaccant
+ *
+ * @author Zhang Zeyu
+ * @date 2022/3/26
+ * @version 1.1
+ * Change font.
  */
 public class WelcomePanel extends JPanel {
     JLabel background;
@@ -32,17 +37,17 @@ public class WelcomePanel extends JPanel {
         a big welcome
          */
         welcomeLabel = new JLabel("Welcome to Beijing International Airport!");
-        welcomeLabel.setBounds(360, 100,1200,200);
-        welcomeLabel.setFont(new Font("Arial", Font.PLAIN, 64));
+        welcomeLabel.setBounds(210, 110,1500,200);
+        welcomeLabel.setFont(new Font("Calibri", Font.BOLD, 80));
         welcomeLabel.setHorizontalAlignment(SwingConstants.CENTER);
         add(welcomeLabel, 0);
         /*
         hint for continuing
          */
-        hintLabel = new JLabel("Click anywhere to start your check-in");
+        hintLabel = new JLabel("click anywhere to start your check-in");
         hintLabel.setBounds(360, 100, 1200, 1600);
-        hintLabel.setFont(new Font("Arial", Font.PLAIN, 36));
-        hintLabel.setForeground(Color.white);
+        hintLabel.setFont(new Font("Calibri", Font.PLAIN, 36));
+        hintLabel.setForeground(Color.lightGray);
         hintLabel.setHorizontalAlignment(SwingConstants.CENTER);
         add(hintLabel, 0);
         /*
