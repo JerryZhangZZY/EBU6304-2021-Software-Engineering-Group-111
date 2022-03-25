67734e0936fbe20d6236ed6d43337056595407cd
lichunlin <2482515293@qq.com>
2022-03-25 11:25:20 +0800

    TypeIfIdLoginCard

---
+++ Kiosk/src/cardTest/TypeIfIdLoginCardTest.java
@@ -0,0 +1,21 @@
+package cardTest;
+
+import card.TypeIfIdLoginCard;
+import frame.MainFrame;
+
+/**
+ * @author Li Chunlin
+ * @version 1.0
+ * @date 3/24
+ * test for log in the surname and id
+ */
+public class TypeIfIdLoginCardTest {
+
+    public static void main(String[] args) {
+        MainFrame testMainFrame = new MainFrame(1);
+        TypeIfIdLoginCard h = new TypeIfIdLoginCard();
+        testMainFrame.loadPanel(h);
+        testMainFrame.displayComponents(true,true,true);
+        testMainFrame.setVisible(true);
+    }
+}
