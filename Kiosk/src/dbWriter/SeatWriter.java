4926329afbc7d741664ab90b5f8107e323126534
Zhang Zeyu <997130886@qq.com>
2022-03-27 14:05:57 +0800

    Finish SeatWriter and its corresponding test class.

---
+++ Kiosk/src/dbWriter/SeatWriter.java
@@ -0,0 +1,47 @@
+package dbWriter;
+
+import com.csvreader.CsvReader;
+import com.csvreader.CsvWriter;
+import dbReader.FlightReader;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class is a tool that can write seat status quickly.
+ *
+ * @author Zhang Zeyu
+ * @date 2022/3/27
+ * @version 1.0
+ */
+
+public abstract class SeatWriter {
+    public static void setSeat(String idFlight, int row, int column) {
+        int idPlane = FlightReader.getIdPlane(FlightReader.indexOf(idFlight));
+        String path = "DB/planes/plane" + idPlane + ".csv";
+        try {
+            CsvReader csvReader = new CsvReader(path);
+            CsvWriter csvWriter = new CsvWriter(path);
+            List<String[]> list = new ArrayList<>();
+
+            for (int currentRow = 1; csvReader.readRecord(); currentRow++) {
+                if (currentRow == row) {
+                    String[] record = csvReader.getValues();
+                    record[column-1] = "X";
+                    list.add(record);
+                } else {
+                    list.add(csvReader.getValues());
+                }
+            }
+            csvReader.close();
+
+            for (String[] strings : list)
+                csvWriter.writeRecord(strings);
+
+            csvWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Kiosk/src/dbWriterTest/SeatWriterTest.java
@@ -0,0 +1,10 @@
+package dbWriterTest;
+
+import dbWriter.SeatWriter;
+
+public class SeatWriterTest {
+
+    public static void main(String[] args) {
+        SeatWriter.setSeat("CA0001", 3, 3);
+    }
+}
