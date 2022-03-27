cb3ed804032995713a389e40f7e6d9e009c8aa24
Zhang Zeyu <997130886@qq.com>
2022-03-27 14:08:30 +0800

    Add javadoc.

--- Kiosk/src/dbWriterTest/SeatWriterTest.java
+++ Kiosk/src/dbWriterTest/SeatWriterTest.java
@@ -2,6 +2,14 @@ package dbWriterTest;
 
 import dbWriter.SeatWriter;
 
+/**
+ * This is the test class of SeatWriter.
+ *
+ * @author Zhang Zeyu
+ * @date 2022/3/27
+ * @version 1.0
+ */
+
 public class SeatWriterTest {
 
     public static void main(String[] args) {
