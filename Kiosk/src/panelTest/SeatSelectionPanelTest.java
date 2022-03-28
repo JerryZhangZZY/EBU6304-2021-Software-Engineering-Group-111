0343768652483740ba65bccb63118ecd9107f0f5
Zhang Zeyu <997130886@qq.com>
2022-03-28 17:26:31 +0800

    Upgrade the test ui.

--- Kiosk/src/panelTest/SeatSelectionPanelTest.java
+++ Kiosk/src/panelTest/SeatSelectionPanelTest.java
@@ -1,6 +1,8 @@
 package panelTest;
 
 import frame.MainFrame;
+import main.State;
+import panel.ProgressPanel;
 import panel.SeatSelectionPanel;
 
 /**
@@ -8,11 +10,23 @@ import panel.SeatSelectionPanel;
  * @author Liang Zhehao
  * @date 2022/3/24
  * @version 1.0
+ *
+ * @author Zhang Zeyu
+ * @date 2022/3/28
+ * @version 1.1
+ * Upgrade the test ui.
  */
 public class SeatSelectionPanelTest {
     public static void main(String[] args) {
+        State.setPassengerName("Jack");
+        State.setBookingNum("bn0001");
+        State.setIdFlight("CA0001");
         MainFrame frame = new MainFrame();
-        frame.loadPanel(new SeatSelectionPanel());
+        ProgressPanel progressPanel = new ProgressPanel(2);
+        SeatSelectionPanel seatSelectionPanel = new SeatSelectionPanel();
+        seatSelectionPanel.add(State.smallBillCard);
+        progressPanel.loadCardsPanel(seatSelectionPanel);
+        frame.loadPanel(progressPanel);
         frame.setVisible(true);
     }
 }
