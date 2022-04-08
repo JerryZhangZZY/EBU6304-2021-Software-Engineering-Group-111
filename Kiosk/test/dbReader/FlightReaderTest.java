5ed5cb46c757119b3e00361930ef61c78f3a03be
Zhang Zeyu <997130886@qq.com>
2022-04-08 15:49:38 +0800

    Finish FlightReaderTest。

---
+++ Kiosk/test/dbReader/FlightReaderTest.java
@@ -0,0 +1,62 @@
+package dbReader;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * This is a unit test of FlightReader.
+ *
+ * @author Zhang Zeyu
+ *
+ * @version 2.0
+ * @date 2022/4/8
+ */
+
+class FlightReaderTest {
+
+    @Test
+    void indexOf() {
+        assertEquals(0, FlightReader.indexOf("CA0001"));
+    }
+
+    @Test
+    void getDeparture() {
+        assertEquals("Beijing T3", FlightReader.getDeparture(0));
+    }
+
+    @Test
+    void getArrival() {
+        assertEquals("Shanghai T2", FlightReader.getArrival(0));
+    }
+
+    @Test
+    void getGate() {
+        assertEquals("3A", FlightReader.getGate(0));
+    }
+
+    @Test
+    void getDepartureTime() {
+        assertEquals("10:00", FlightReader.getDepartureTime(0));
+    }
+
+    @Test
+    void getArrivalTime() {
+        assertEquals("12:00", FlightReader.getArrivalTime(0));
+    }
+
+    @Test
+    void getBoardingTime() {
+        assertEquals("09:30", FlightReader.getBoardingTime(0));
+    }
+
+    @Test
+    void getIdPlane() {
+        assertEquals(1, FlightReader.getIdPlane(0));
+    }
+
+    @Test
+    void getDate() {
+        assertEquals("2022-03-18", FlightReader.getDate(0));
+    }
+}
