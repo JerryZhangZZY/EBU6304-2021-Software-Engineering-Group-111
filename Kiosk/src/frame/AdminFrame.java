e6e44dd42ab5994f6f558b08263e2f75ad91b143
ZaitianWang <wangzaitian@outlook.com>
2022-04-13 10:52:25 +0800

    interaction improvement

--- Kiosk/src/frame/AdminFrame.java
+++ Kiosk/src/frame/AdminFrame.java
@@ -9,6 +9,13 @@ import java.awt.event.KeyEvent;
 
 import static java.lang.Thread.sleep;
 
+/**
+ * small window for admin to exit the programme
+ * @author zaitian
+ *
+ * @version 1.0
+ * @date 4/11
+ */
 public class AdminFrame extends JFrame {
     JPanel contentPane = new JPanel();
 
@@ -27,6 +34,7 @@ public class AdminFrame extends JFrame {
         terminal.setForeground(Color.white);
         terminal.setCursor(new Cursor(Cursor.TEXT_CURSOR));
         terminal.setFont(new Font("Consolas", Font.PLAIN, 16));
+        terminal.setText("To resume checking-in, enter \"resumo\"\n");
         contentPane.add(terminal);
 
         terminal.addKeyListener(new KeyAdapter() {
