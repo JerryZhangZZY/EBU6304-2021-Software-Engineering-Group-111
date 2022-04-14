50aa37c22aec014ef5a61cc035669971bf8928c2
LIANGZH <jp2019213471@qmul.ac.uk>
2022-04-14 15:19:24 +0800

    #12 fix bugs

--- Kiosk/JUnitTests/panel/WelcomePanelTest.java
+++ Kiosk/JUnitTests/panel/WelcomePanelTest.java
@@ -4,12 +4,8 @@ import frame.MainFrame;
 import main.State;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
-import org.w3c.dom.ranges.Range;
-import panel.WelcomePanel;
-import org.junit.jupiter.api.Test;
 
 import java.awt.*;
-import java.awt.dnd.MouseDragGestureRecognizer;
 import java.awt.event.InputEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -20,6 +16,11 @@ import static org.junit.jupiter.api.Assertions.*;
 /**
  * test class for welcome panel
  * @author zaitian
+ * @author Liang Zhehao
+ *
+ * @version 2.0
+ * @date 4/14
+ *
  * @version 1.0
  * @date 4/8
  */
@@ -27,39 +28,43 @@ class WelcomePanelTest {
     MainFrame mainFrame = new MainFrame();
     WelcomePanel welcomePanel = new WelcomePanel();
     @BeforeEach
-    void reset(){
-        State.setPc(0);
+    void reset() throws InterruptedException {
+        mainFrame.lockScreen();
+        Thread.sleep(5000);
     }
-    @RepeatedTest(10)
-    void test(){
-        mainFrame.hideBars(true);
-        mainFrame.loadPanel(welcomePanel);
-        mainFrame.setVisible(true);
-        try {
-            Robot robot = new Robot();
-            while(!mainFrame.isVisible())
-                ;
-            Point p = MouseInfo.getPointerInfo().getLocation();
-            robot.mouseMove(new Random().nextInt(1920),
-                    new Random().nextInt(1080));
-            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-            robot.mouseMove(p.x, p.y);
-            welcomePanel.getMask().addMouseListener(new MouseAdapter() {
-                @Override
-                public void mouseReleased(MouseEvent e) {
-                    super.mouseReleased(e);
-                    mainFrame.setVisible(false);
+    @RepeatedTest(2)
+    void test() {
+        for (int j = 0; j < 10; j++) {
+            State.setPc(0);
+            mainFrame.hideBars(true);
+            mainFrame.setVisible(true);
+            try {
+                Robot robot = new Robot();
+                while(!mainFrame.isVisible())
+                    ;
+                Point p = MouseInfo.getPointerInfo().getLocation();
+                robot.mouseMove(new Random().nextInt(1920),
+                        new Random().nextInt(1080));
+                robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+                robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+                robot.mouseMove(p.x, p.y);
+                welcomePanel.getMask().addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseReleased(MouseEvent e) {
+                        super.mouseReleased(e);
+                        mainFrame.setVisible(false);
+                    }
+                });
+                for (int i = 0; i < 1000; i++) {
+                    if (State.getPc() != 1)
+                        robot.delay(1);
                 }
-            });
-            for (int i = 0; i < 1000; i++) {
-                if (State.getPc() != 1)
-                    robot.delay(1);
+                assertEquals(1, State.getPc());
+            } catch (AWTException e) {
+                e.printStackTrace();
             }
-            assertEquals(1, State.getPc());
-        } catch (AWTException e) {
-            e.printStackTrace();
         }
+
     }
 
 
