83ef4bb2be4b5f5c622d0c790e7e40e5efeac106
Zhang Zeyu <997130886@qq.com>
2022-04-18 20:03:13 +0800

    Delete a useless GUI test.

--- Kiosk/guiTests/frameTest/AdminFrameTest.java
+++
@@ -1,9 +0,0 @@
-package frameTest;
-
-import frame.AdminFrame;
-
-public class AdminFrameTest {
-    public static void main(String[] args) {
-        AdminFrame adminFrame = new AdminFrame();
-    }
-}
