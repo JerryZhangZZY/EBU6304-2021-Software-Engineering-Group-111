de49ddec0a15585d41864db765b9a2dbeba6218a
Zhang Zeyu <997130886@qq.com>
2022-04-24 00:16:26 +0800

    Add javadoc.

--- Kiosk/guiTests/panelTest/WelcomePanelTest.java
+++ Kiosk/guiTests/panelTest/WelcomePanelTest.java
@@ -2,6 +2,20 @@ package panelTest;
 
 import frame.MainFrame;
 
+/**
+ * This class tests the GUI of WelcomePanel.
+ *
+ * @author Wang Zaitian
+ * @author Zhang Zeyu
+ *
+ * @version 3.0
+ * Upgrade.
+ * @date 2022/4/24
+ *
+ * @version 1.0
+ * @date 2022/3/24
+ */
+
 public class WelcomePanelTest {
     public static void main(String[] args) {
         MainFrame mainFrame = new MainFrame();
