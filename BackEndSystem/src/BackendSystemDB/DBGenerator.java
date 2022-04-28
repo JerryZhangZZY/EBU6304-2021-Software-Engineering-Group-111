04ae76b9ee589065a92347f9dd63f9aceb304476
Ruijie Ni <1924975712@qq.com>
2022-04-28 16:07:37 +0800

    Fixed #55

--- BackEndSystem/src/BackendSystemDB/DBGenerator.java
+++ BackEndSystem/src/BackendSystemDB/DBGenerator.java
@@ -150,19 +150,15 @@ public class DBGenerator {
 
     public static void main(String[] args) throws Exception {
         Random r = new Random();
-        //第一步：设置输出的文件路径
-        //如果该目录下不存在该文件，则文件会被创建到指定目录下。如果该目录有同名文件，那么该文件将被覆盖。
         File writeFile = new File("DB/backend.csv");
 
         try {
-            //第二步：通过BufferedReader类创建一个使用默认大小输出缓冲区的缓冲字符输出流
             BufferedWriter writeText = new BufferedWriter(new FileWriter(writeFile));
 
             writeText.write("idPassenger,surName,idFlight,bookingNum,status,seat,meal,mealPre1,mealPre2,mealPre3,airline");
             for (int i = 0; i < 16; i++) {
                 getInfo();
-                writeText.newLine();    //换行
-                //调用write的方法将字符串写到流中
+                writeText.newLine();
                 writeText.write(idPassenger + separator + surName + separator + idFlight + separator + bookingNum + separator + 0 + separator + "NULL" + separator + "NULL" + separator + "NULL" + separator + "NULL" + separator + "NULL" + separator + airline);
                 index++;
             }
@@ -172,16 +168,13 @@ public class DBGenerator {
                 writeText.newLine();
                 writeText.write(idPassenger + separator + surName + separator + idFlight + separator + bookingNum + separator + 1 + separator + seat + separator + meal + separator + mealPre1 + separator + mealPre2 + separator + mealPre3 + separator + airline);
             }
-            //使用缓冲区的刷新方法将数据刷到目的地中
             writeText.flush();
-            //关闭缓冲区，缓冲区没有调用系统底层资源，真正调用底层资源的是FileWriter对象，缓冲区仅仅是一个提高效率的作用
-            //因此，此处的close()方法关闭的是被缓存的流对象
             writeText.close();
         } catch (FileNotFoundException e) {
-            System.out.println("没有找到指定文件");
+            System.out.println("No such file");
             e.printStackTrace();
         } catch (IOException e) {
-            System.out.println("文件读写出错");
+            System.out.println("Error at reading file");
         }
     }
 
