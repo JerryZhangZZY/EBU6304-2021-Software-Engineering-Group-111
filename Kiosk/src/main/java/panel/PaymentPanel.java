509473ea90cf6a3a8b3eec31c281c3523299f54f
Zhang Zeyu <997130886@qq.com>
2022-05-12 17:24:11 +0800

    negligible

--- Kiosk/src/main/java/panel/PaymentPanel.java
+++ Kiosk/src/main/java/panel/PaymentPanel.java
@@ -174,9 +174,7 @@ public class PaymentPanel extends JPanel {
         });
         mouseListener = new MouseListener() {
             @Override
-            public void mouseClicked(MouseEvent e) {
-
-            }
+            public void mouseClicked(MouseEvent e) {}
 
             @Override
             public void mousePressed(MouseEvent e) {
@@ -191,14 +189,10 @@ public class PaymentPanel extends JPanel {
             }
 
             @Override
-            public void mouseEntered(MouseEvent e) {
-
-            }
+            public void mouseEntered(MouseEvent e) {}
 
             @Override
-            public void mouseExited(MouseEvent e) {
-
-            }
+            public void mouseExited(MouseEvent e) {}
         };
         btnPay.addMouseListener(mouseListener);
 
