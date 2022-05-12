3d0296c850d57e37087d877f7afc055b6fdccc4d
LIANGZH <jp2019213471@qmul.ac.uk>
2022-05-12 14:19:08 +0800

    enhance test

--- Kiosk/src/test/java/JUnitTests/panel/PaymentPanelTest.java
+++ Kiosk/src/test/java/JUnitTests/panel/PaymentPanelTest.java
@@ -1,4 +1,5 @@
 package panel;
+
 import common.MainFrameBarsTest;
 import frame.MainFrame;
 import main.State;
@@ -12,7 +13,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * test class for id card scanning panel
+ *
  * @author Chenyu
+ * @author Liang Zhehao
+ *
+ * @version 4.0
+ * @date 5/12
  *
  * @version 3.0
  * implement MainFrameBarsTest interface
@@ -20,57 +26,62 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  */
 public class PaymentPanelTest implements MainFrameBarsTest {
     private int price;
-    private MainFrame mainFrame= new MainFrame();
+    private MainFrame mainFrame = new MainFrame();
     int currentPc;
+
     @BeforeEach
-    public void reset(){
+    public void reset() {
         currentPc = 7;
         State.setPc(7);
     }
+
     @Test
-    public void testPaymentPanel(){
+    public void testPaymentPanel() {
         price = State.getBill();
         PaymentPanel test = new PaymentPanel(price);
         JButton pay = test.getButtonPay();
-        JTextField id  =test.getTfCreditId();
+        JTextField id = test.getTfCreditId();
         id.setText("12345678");
-        currentPc  =  State.getPc();
+        currentPc = State.getPc();
         pay.doClick();
-        assertEquals(currentPc+1, State.getPc());
+        assertEquals(currentPc + 1, State.getPc());
     }
+
     @Test
-    public void testChecksum(){
+    public void testChecksum() {
         PaymentPanel test = new PaymentPanel(price);
         JButton pay = test.getButtonPay();
-        JTextField id  =test.getTfCreditId();
-        JLabel error  =test.getErrorWarning();
+        JTextField id = test.getTfCreditId();
+        JLabel error = test.getErrorWarning();
         id.setText("123456789");
-        currentPc  =  State.getPc();
+        currentPc = State.getPc();
         pay.doClick();
-        assertEquals(currentPc,State.getPc());
+        assertEquals(currentPc, State.getPc());
         assertTrue(error.isVisible());
         id.setText("123456llplp9");
-        currentPc  =  State.getPc();
+        currentPc = State.getPc();
         pay.doClick();
-        assertEquals(currentPc,State.getPc());
+        assertEquals(currentPc, State.getPc());
         assertTrue(error.isVisible());
         id.setText("");
-        currentPc  =  State.getPc();
+        currentPc = State.getPc();
         pay.doClick();
-        assertEquals(currentPc,State.getPc());
+        assertEquals(currentPc, State.getPc());
         assertTrue(error.isVisible());
         id.setText("12345678");
-        currentPc  =  State.getPc();
+        currentPc = State.getPc();
         pay.doClick();
-        assertEquals(currentPc+1,State.getPc());
+        assertEquals(currentPc + 1, State.getPc());
         assertTrue(error.isVisible());
     }
+
     @Test
-    public void testExit(){
+    public void testExit() {
         doExit(mainFrame);
     }
+
     @Test
-    public void testBack(){
-        doBack(mainFrame,currentPc);
+    public void testBack() {
+        doBack(mainFrame, currentPc);
     }
 }
