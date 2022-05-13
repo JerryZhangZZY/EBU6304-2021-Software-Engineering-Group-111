92c9e301d14dbc5c36402d5109a99a03e25be5ee
ignore <nosucheamil@softwareengineer.com>
2022-05-13 12:42:11 +0800

    Fix #72 using the ignored user.

---
+++ kiosk/src/test/java/guiTests/cardTest/PlaneInfoCardTest.java
@@ -0,0 +1,27 @@
+package cardTest;
+
+import card.PlaneInfoCard;
+import frame.MainFrame;
+import main.State;
+
+/**
+ * This class test PlaneInfoCard
+ *
+ * @author Liang Zhehao
+ *
+ * @version 4.0
+ * @date 2022/5/7
+ */
+public class PlaneInfoCardTest {
+
+    public static void main(String[] args) {
+
+        State.setIdFlight("AC0001");
+        MainFrame testMainFrame = new MainFrame();
+        PlaneInfoCard planeInfoCard = new PlaneInfoCard();
+        planeInfoCard.setLocation(0, 0);
+        testMainFrame.loadPanel(planeInfoCard);
+        testMainFrame.displayComponents(true, true, false);
+        testMainFrame.setVisible(true);
+    }
+}
