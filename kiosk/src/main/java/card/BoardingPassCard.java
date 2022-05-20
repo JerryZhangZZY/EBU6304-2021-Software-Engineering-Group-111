f91bcb7e7fc884013018d7796f273fb5d1da9850
Zhang Zeyu <997130886@qq.com>
2022-05-20 11:07:54 +0800

    #79 amend boarding pass background and improve BoardingPassCard layout.

--- kiosk/src/main/java/card/BoardingPassCard.java
+++ kiosk/src/main/java/card/BoardingPassCard.java
@@ -13,6 +13,10 @@ import java.awt.*;
  * @author Zhang Zeyu
  * @author Liang Zhehao
  *
+ * @version 5.1
+ * Amend layout.
+ * @date 2022/5/20
+ *
  * @version 5.0
  * Modify Layout
  * Appearance improvement
@@ -43,13 +47,12 @@ public class BoardingPassCard extends JPanel {
         String departureCity = "", departureTerminal = "";
         for (int i = 0; i < CnT1.length - 1; i++)
             departureCity = departureCity + CnT1[i] + " ";
-        departureTerminal = CnT1[CnT1.length - 1];
+        departureTerminal = CnT1[CnT1.length - 1].replace("T", "");
         String arrival = FlightReader.getArrival(indexFlight);
         String[] CnT2 = arrival.split(" ");
-        String arrivalCity = "",  arrivalTerminal = "";
+        String arrivalCity = "";
         for (int i = 0; i < CnT2.length - 1; i++)
             arrivalCity = arrivalCity + CnT2[i] + " ";
-        arrivalTerminal = CnT1[CnT2.length - 1];
         String airline = PlaneReader.getAirline(indexPlane);
         String gate = FlightReader.getGate(indexFlight);
         String boardingTime = FlightReader.getBoardingTime(indexFlight);
@@ -59,125 +62,126 @@ public class BoardingPassCard extends JPanel {
         setSize(1120,300);
 
         JLabel lblAirline1 = new JLabel(airline);
-        lblAirline1.setBounds(580, 0, 200, 55);
-        lblAirline1.setFont(new Font("Arial", Font.PLAIN, 24));
+        lblAirline1.setBounds(570, 0, 200, 48);
+        lblAirline1.setFont(new Font("Arial", Font.PLAIN, 22));
         lblAirline1.setForeground(new Color(255, 255, 255));
         lblAirline1.setVerticalAlignment(SwingConstants.CENTER);
         lblAirline1.setHorizontalAlignment(SwingConstants.RIGHT);
         add(lblAirline1);
 
         JLabel lblAirline2 = new JLabel(airline);
-        lblAirline2.setBounds(810, 0, 300, 55);
-        lblAirline2.setFont(new Font("Arial", Font.PLAIN, 24));
+        lblAirline2.setBounds(875, 0, 200, 48);
+        lblAirline2.setFont(new Font("Arial", Font.PLAIN, 22));
         lblAirline2.setForeground(new Color(255, 255, 255));
         lblAirline2.setVerticalAlignment(SwingConstants.CENTER);
-        lblAirline2.setHorizontalAlignment(SwingConstants.CENTER);
+        lblAirline2.setHorizontalAlignment(SwingConstants.LEADING);
         add(lblAirline2);
 
-        JLabel lblName1 = new JLabel(State.getPassengerName());
-        lblName1.setBounds(54, 142, 100, 25);
-        lblName1.setFont(new Font("Arial", Font.BOLD, 14));
-        lblName1.setForeground(new Color(28, 28, 28));
-        add(lblName1);
+        JLabel lblPassengerName1 = new JLabel(State.getPassengerName());
+        lblPassengerName1.setBounds(49, 141, 100, 25);
+        lblPassengerName1.setFont(new Font("Arial", Font.BOLD, 19));
+        lblPassengerName1.setForeground(new Color(28, 28, 28));
+        add(lblPassengerName1);
 
-        JLabel lblName2 = new JLabel(State.getPassengerName());
-        lblName2.setBounds(873, 60, 100, 25);
-        lblName2.setFont(new Font("Arial", Font.BOLD, 14));
-        lblName2.setForeground(new Color(28, 28, 28));
-        add(lblName2);
+        JLabel lblPassengerName2 = new JLabel(State.getPassengerName());
+        lblPassengerName2.setBounds(876, 59, 100, 25);
+        lblPassengerName2.setFont(new Font("Arial", Font.BOLD, 19));
+        lblPassengerName2.setForeground(new Color(28, 28, 28));
+        add(lblPassengerName2);
 
         JLabel lblDeparture1 = new JLabel(departureCity);
         lblDeparture1.setForeground(new Color(28, 28, 28));
-        lblDeparture1.setFont(new Font("Arial", Font.BOLD, 20));
-        lblDeparture1.setBounds(54, 75, 200, 25);
+        lblDeparture1.setFont(new Font("Arial", Font.BOLD, 28));
+        lblDeparture1.setBounds(49, 67, 200, 35);
         add(lblDeparture1);
 
         JLabel lblDeparture2 = new JLabel(departureCity);
         lblDeparture2.setForeground(new Color(28, 28, 28));
-        lblDeparture2.setFont(new Font("Arial", Font.BOLD, 14));
-        lblDeparture2.setBounds(918, 146, 200, 25);
+        lblDeparture2.setFont(new Font("Arial", Font.BOLD, 19));
+        lblDeparture2.setBounds(924, 143, 200, 25);
         add(lblDeparture2);
 
         JLabel lblDepartureTime = new JLabel(departureTime);
         lblDepartureTime.setForeground(new Color(28, 28, 28));
-        lblDepartureTime.setFont(new Font("Arial", Font.BOLD, 14));
-        lblDepartureTime.setBounds(918, 169, 200, 25);
+        lblDepartureTime.setFont(new Font("Arial", Font.BOLD, 19));
+        lblDepartureTime.setBounds(924, 164, 200, 25);
         add(lblDepartureTime);
 
         JLabel lblArrival1 = new JLabel(arrivalCity);
         lblArrival1.setForeground(new Color(28, 28, 28));
-        lblArrival1.setFont(new Font("Arial", Font.BOLD, 20));
-        lblArrival1.setBounds(294, 75, 200, 25);
+        lblArrival1.setFont(new Font("Arial", Font.BOLD, 28));
+        lblArrival1.setBounds(291, 67, 200, 35);
         add(lblArrival1);
 
         JLabel lblArrival2 = new JLabel(arrivalCity);
         lblArrival2.setForeground(new Color(28, 28, 28));
-        lblArrival2.setFont(new Font("Arial", Font.BOLD, 14));
-        lblArrival2.setBounds(918, 192, 200, 25);
+        lblArrival2.setFont(new Font("Arial", Font.BOLD, 19));
+        lblArrival2.setBounds(924, 189, 200, 25);
         add(lblArrival2);
 
         JLabel lblArrivalTime = new JLabel(arrivalTime);
         lblArrivalTime.setForeground(new Color(28, 28, 28));
-        lblArrivalTime.setFont(new Font("Arial", Font.BOLD, 14));
-        lblArrivalTime.setBounds(918, 215, 200, 25);
+        lblArrivalTime.setFont(new Font("Arial", Font.BOLD, 19));
+        lblArrivalTime.setBounds(924, 210, 200, 25);
         add(lblArrivalTime);
 
         JLabel lblDate1 = new JLabel(date);
-        lblDate1.setFont(new Font("Arial", Font.BOLD, 14));
         lblDate1.setForeground(new Color(28, 28, 28));
-        lblDate1.setBounds(54, 185, 274, 25);
+        lblDate1.setFont(new Font("Arial", Font.BOLD, 19));
+        lblDate1.setBounds(49, 184, 274, 25);
         add(lblDate1);
 
         JLabel lblDate2 = new JLabel(date);
-        lblDate2.setFont(new Font("Arial", Font.BOLD, 14));
         lblDate2.setForeground(new Color(28, 28, 28));
-        lblDate2.setBounds(873, 103, 274, 25);
+        lblDate2.setFont(new Font("Arial", Font.BOLD, 19));
+        lblDate2.setBounds(876, 101, 274, 25);
         add(lblDate2);
 
-        JLabel lblSeat = new JLabel(seat);
-        lblSeat.setFont(new Font("Arial", Font.BOLD, 14));
-        lblSeat.setForeground(new Color(28, 28, 28));
-        lblSeat.setBounds(54, 243, 274, 25);
-        add(lblSeat);
+        JLabel lblClass = new JLabel(seat);
+        lblClass.setFont(new Font("Arial", Font.BOLD, 19));
+        lblClass.setForeground(new Color(28, 28, 28));
+        lblClass.setBounds(49, 242, 274, 25);
+        add(lblClass);
 
         JLabel lblIdFlight = new JLabel(idFlight);
         lblIdFlight.setForeground(new Color(28, 28, 28));
-        lblIdFlight.setFont(new Font("Arial", Font.BOLD, 20));
-        lblIdFlight.setBounds(510, 75, 237, 25);
+        lblIdFlight.setFont(new Font("Arial", Font.BOLD, 28));
+        lblIdFlight.setBounds(508, 67, 237, 35);
         add(lblIdFlight);
 
         JLabel lblTerminal = new JLabel(departureTerminal);
         lblTerminal.setForeground(new Color(28, 28, 28));
-        lblTerminal.setFont(new Font("Arial", Font.BOLD, 20));
-        lblTerminal.setBounds(671, 75, 237, 25);
+        lblTerminal.setFont(new Font("Arial", Font.BOLD, 28));
+        lblTerminal.setBounds(673, 67, 237, 35);
         add(lblTerminal);
 
         JLabel lblGate = new JLabel(gate);
-        lblGate.setBounds(673, 155, 300, 25);
-        lblGate.setFont(new Font("Arial", Font.BOLD, 20));
         lblGate.setForeground(new Color(28, 28, 28));
+        lblGate.setFont(new Font("Arial", Font.BOLD, 28));
+        lblGate.setBounds(673, 147, 300, 35);
         add(lblGate);
 
         JLabel lblBoardingTime = new JLabel(boardingTime);
-        lblBoardingTime.setBounds(509, 155, 300, 25);
-        lblBoardingTime.setFont(new Font("Arial", Font.BOLD, 20));
         lblBoardingTime.setForeground(new Color(28, 28, 28));
+        lblBoardingTime.setFont(new Font("Arial", Font.BOLD, 28));
+        lblBoardingTime.setBounds(508, 147, 300, 35);
         add(lblBoardingTime);
 
         JLabel lblSeatNum1 = new JLabel("" + State.getSeatRow() + PlaneReader.getColumnNum(indexPlane).split(",")[State.getSeatColumn() - 1]);
-        lblSeatNum1.setBounds(509, 239, 300, 25);
-        lblSeatNum1.setFont(new Font("Arial", Font.BOLD, 20));
         lblSeatNum1.setForeground(new Color(28, 28, 28));
+        lblSeatNum1.setFont(new Font("Arial", Font.BOLD, 28));
+        lblSeatNum1.setBounds(508, 233, 300, 35);
         add(lblSeatNum1);
 
         JLabel lblSeatNum2 = new JLabel("" + State.getSeatRow() + PlaneReader.getColumnNum(indexPlane).split(",")[State.getSeatColumn() - 1]);
-        lblSeatNum2.setBounds(921, 244, 300, 25);
-        lblSeatNum2.setFont(new Font("Arial", Font.BOLD, 20));
         lblSeatNum2.setForeground(new Color(28, 28, 28));
+        lblSeatNum2.setFont(new Font("Arial", Font.BOLD, 28));
+        lblSeatNum2.setBounds(924, 238, 300, 35);
         add(lblSeatNum2);
+    }
 
-        JLabel background = new JLabel(new ImageIcon("kiosk/icons/boardingPass.png"));
-        background.setBounds(0, 0, 1120, 300);
-        add(background);
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(new ImageIcon("kiosk/icons/boardingPass.png").getImage(), 0, 0,this.getWidth(), this.getHeight(), this);
     }
 }
--- kiosk/src/test/java/guiTests/cardTest/BoardingPassCardTest.java
+++ kiosk/src/test/java/guiTests/cardTest/BoardingPassCardTest.java
@@ -8,15 +8,24 @@ import main.State;
  * This is the test class of BoardingPassCard.
  *
  * @author Zhang Zeyu
- * @date 2022/3/24
+ *
+ * @version 5.0
+ * Adapt to the latest software version.
+ * @date 2022/5/20
+ *
  * @version 1.0
+ * @date 2022/3/24
  */
 
 public class BoardingPassCardTest {
     public static void main(String[] args) {
-        State.setSeatColumn(1);
+        State.setSeatColumn(3);
+        State.setSeatRow(8);
+        State.setPassengerName("Jack");
         MainFrame frame = new MainFrame();
-        frame.loadPanel(new BoardingPassCard("CA0001"));
+        BoardingPassCard boardingPassCard = new BoardingPassCard("CA0001");
+        boardingPassCard.setLocation(100, 100);
+        frame.loadPanel(boardingPassCard);
         frame.setVisible(true);
     }
 }
